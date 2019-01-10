710aa007aa33edd6ea12b3d3c258b58c6eefd0b1
Ricardo Belchior <ricardo.belchior@reachplc.com>
2019-01-10 17:08:51 +0000

    Migrate to AndroidX

--- sample/src/main/java/com/rbelchior/tmbutton/sample/FakeList.java
+++ sample/src/main/java/com/rbelchior/tmbutton/sample/FakeList.java
@@ -1,8 +1,8 @@
 package com.rbelchior.tmbutton.sample;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.TooltipCompat;
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.TooltipCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- sample/src/main/java/com/rbelchior/tmbutton/sample/SampleActivity.java
+++ sample/src/main/java/com/rbelchior/tmbutton/sample/SampleActivity.java
@@ -1,7 +1,7 @@
 package com.rbelchior.tmbutton.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ListView;
 
 import com.rbelchior.tmbutton.TMButton;
